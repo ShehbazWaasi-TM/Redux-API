7799659e2b6b06b038ecc53c1671e2eee385f3ec
ShehbazWaasi20 <shehbazwaasi@gmail.com>
2022-07-25 23:44:04 +0530

    fetch api

--- redux-and-api/src/App.js
+++ redux-and-api/src/App.js
@@ -1,57 +1,17 @@
 import './App.css';
-import {React, useEffect, useState} from "react";
+import {React, useState} from "react";
+import Fetchapi from './components/Fetchapi';
 
 function App() {
   const [count,setCount] = useState(0)
-  const [contacts, setContacts] = useState()
-
-    const Data = async()=>{
-      const gethata = await fetch("https://jsonplaceholder.typicode.com/users",{
-        method:"GET"
-      })
-      const res = await gethata.json()
-      console.log(res)
-      setContacts(res)    
-    }
-  useEffect(()=>{
-    Data()
-  },[])
+ 
   return (
     <div className="App">
       <p>{count}</p>
       <button onClick={()=>setCount(count+1)}>Increment</button>
       <button onClick={()=>setCount(count-1)}>Decrement</button>
       <button onClick={()=>setCount(0)}>Reset</button>
-      <table border="1px">
-        <thead>
-          <tr>
-            <th>S.no</th>
-            <th>name</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Company</th>
-            <th>Phone</th>
-            <th>Address</th>
-            <th>Website</th>
-          </tr>
-        </thead>
-        <tbody>
-        {contacts.map((contact)=>{
-              return (
-                <tr key={contact.id}>
-                  <td>{contact.id}</td>
-                  <td>{contact.name}</td>
-                  <td>{contact.username}</td>
-                  <td>{contact.email}</td>
-                  <td>{contact.company.name}</td>
-                  <td>{contact.phone}</td>
-                  <td>{contact.address.street}</td>
-                  <td>{contact.website}</td>
-                </tr>
-              )
-            })}
-        </tbody>
-      </table>
+      <Fetchapi/>
     </div>
   );
 }
---
+++ redux-and-api/src/components/Fetchapi.js
@@ -0,0 +1,53 @@
+import {React, useEffect, useState} from "react";
+
+const Fetchapi = () => {
+    const [contacts, setContacts] = useState()
+
+    const Data = async()=>{
+      const gethata = await fetch("https://jsonplaceholder.typicode.com/users",{
+        method:"GET"
+      })
+      const res = await gethata.json()
+      console.log(res)
+      setContacts(res)    
+    }
+  useEffect(()=>{
+    Data()
+  },[])
+  return (
+    <div>
+      <table border="1px">
+        <thead>
+          <tr>
+            <th>S.no</th>
+            <th>name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Company</th>
+            <th>Phone</th>
+            <th>Address</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+        <tbody>
+        {contacts && contacts.map((contact)=>{
+              return (
+                <tr key={contact.id}>
+                  <td>{contact.id}</td>
+                  <td>{contact.name}</td>
+                  <td>{contact.username}</td>
+                  <td>{contact.email}</td>
+                  <td>{contact.company.name}</td>
+                  <td>{contact.phone}</td>
+                  <td>{contact.address.street}</td>
+                  <td>{contact.website}</td>
+                </tr>
+              )
+            })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Fetchapi
